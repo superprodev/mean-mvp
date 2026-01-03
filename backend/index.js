7707b51096b4632593116fd42c1acb944c5756a4
superprodev <yurgentcollin@outlook.com>
2026-01-02 21:38:25 -0600

    backend added

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+
+const PORT = 8000;
+const db = "mongodb://";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+app.use((req, res) => {
+    res.send({ msg: "Hello World!"});
+})
+
+mongoose.connect(db).then(() => {
+    console.log("MongoDB is connected.");
+}).catch(err => {
+    console.log(err);
+})
+
+app.listen(PORT, (err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(`Server is running at ${PORT}`);
+})
