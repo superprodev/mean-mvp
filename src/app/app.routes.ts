da4c9f893a0da009391497a134ac106764067292
superprodev <yurgentcollin@outlook.com>
2026-01-04 15:39:11 -0600

    admin initialized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,18 @@ import { Signup } from './pages/auth/signup/signup';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { UserInfo } from './pages/auth/user-info/user-info';
 import { Verify } from './pages/auth/verify/verify';
+import { Chat } from './pages/dashboard/chat/chat';
+import { UserList } from './pages/admin/user-list/user-list';
 
 export const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "dashboard" },
+    { path: "admin/users", component: UserList },
+    { path: "admin/users/:id", component: UserInfo },
     { path: "signin", component: Signin },
     { path: "signup", component: Signup },
     { path: "settings", component: UserInfo },
     { path: "verify-email", component: Verify },
     { path: "dashboard", component: Dashboard },
+    { path: "chat", component: Chat },
     { path: "*", redirectTo: "dashboard" }
 ];
